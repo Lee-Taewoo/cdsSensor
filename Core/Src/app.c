f5b3a2595c9d8cff5f519ac00e09a7e507794d12
Lee-Taewoo <liverforever@kookmin.ac.kr>
2025-01-22 13:48:13 +0900

    평균필터

--- Core/Src/app.c
+++ Core/Src/app.c
@@ -10,25 +10,30 @@
 // 외부 장치 선언
 extern ADC_HandleTypeDef hadc1;
 extern UART_HandleTypeDef huart2;
+extern TIM_HandleTypeDef htim3;
+
+uint16_t adcValue[2];
+uint32_t adcSum = 0;
+uint16_t adcDone = 0;
+
+// 타이머가 리셋될 때 발생되는 인터럽트
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+	adcSum += adcValue[1];
+	if(adcDone > 0) {
+		adcDone--;
+	}
+}
 
 void app() {
 	// 시리얼 포트 초기화
 	initUart(&huart2);
+	HAL_ADC_Start_DMA(&hadc1, (uint32_t*)adcValue, 2);
+	HAL_TIM_Base_Start_IT(&htim3);
 	while(1) {
-		// adc 변환 시작
-		HAL_ADC_Start(&hadc1);
-		// adc 대기
-		HAL_ADC_PollForConversion(&hadc1, 1);
-		// 값 읽기
-		uint16_t value1 = HAL_ADC_GetValue(&hadc1);
-		// adc 종료
-		// adc 대기
-		HAL_ADC_PollForConversion(&hadc1, 1);
-		// 값 읽기
-		uint16_t value2 = HAL_ADC_GetValue(&hadc1);
-		// adc 종료
-		HAL_ADC_Stop(&hadc1);
-		printf("cds value = %d %d\n", value1, value2);
-		HAL_Delay(100);
+		if (adcDone == 0) {
+			printf("%ld\n", adcSum / 100);
+			adcSum = 0;
+			adcDone = 100;
+		}
 	}
 }
