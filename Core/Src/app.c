d0e2cad89003daf700734f8104d2a653a6632551
Lee-Taewoo <liverforever@kookmin.ac.kr>
2025-01-22 16:45:29 +0900

    필터 모음집

--- Core/Src/app.c
+++ Core/Src/app.c
@@ -6,6 +6,7 @@
  */
 
 #include "app.h"
+#include <stdbool.h>
 
 // 외부 장치 선언
 extern ADC_HandleTypeDef hadc1;
@@ -13,12 +14,49 @@ extern UART_HandleTypeDef huart2;
 extern TIM_HandleTypeDef htim3;
 
 uint16_t adcValue[2];
-uint32_t adcSum = 0;
 uint16_t adcDone = 0;
+char str[50];
 
+uint16_t movingMean (uint16_t inValue) {
+#define max 100
+	static uint16_t buffer[max] = {0,};
+	static uint32_t sum = 0;
+	static uint16_t pos = 0;
+	static int isFirst = 5;
+	if (isFirst) {
+		isFirst--;
+		for(int i = 0; i < max; i++) {
+			buffer[i] = inValue;
+		}
+		sum = inValue * 100;
+	}
+	sum -= buffer[pos];
+	buffer[pos] = inValue;
+	sum += buffer[pos];
+	pos++;
+	pos %= 100;
+	return sum/100;
+}
+
+double Kalman(double measurement) { // Kalman filter
+	// setup
+	static double P = 1.0;
+	static double varP = 0.0001;
+	static double R = 0.25;
+	static double K = 1.0;
+	static double X = 20.0;
+
+	// simple filter
+	P = P + varP;
+	K = P / (P + R);
+	X = (K* measurement) + (1 - K) * X;
+	P = (1 - K) * P;
+
+	return X;
+}
 // 타이머가 리셋될 때 발생되는 인터럽트
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
-	adcSum += adcValue[1];
+	sprintf(str, "%d, %d, %d\n", adcValue[1], movingMean(adcValue[1]), (int)Kalman(adcValue[1]));
 	if(adcDone > 0) {
 		adcDone--;
 	}
@@ -31,9 +69,8 @@ void app() {
 	HAL_TIM_Base_Start_IT(&htim3);
 	while(1) {
 		if (adcDone == 0) {
-			printf("%ld\n", adcSum / 100);
-			adcSum = 0;
-			adcDone = 100;
+			printf(str);
+			adcDone = 1;
 		}
 	}
 }
